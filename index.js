baeafc7bd57f706a93e4ccb8c68d32c62bf6661a
S M <malapit66@gmail.com>
2020-07-18 22:58:25 -0500

    installed express and added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`App is listening on port ${PORT}`);
+});
