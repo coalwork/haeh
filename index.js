b5e98fcdaa990a2d6fbbbc85d2515c19c689be3d
S M <malapit66@gmail.com>
2020-07-31 02:17:49 -0500

    created login endpoint

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const { v4: uuid } = require('uuid');
+const { Strategy: LocalStrategy } = require('passport-local');
+const { body, validationResult } = require('express-validator');
 const fs = require('fs');
 const ejs = require('ejs');
 const path = require('path');
 const bcrypt = require('bcryptjs');
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
-const { body, validationResult } = require('express-validator');
+const LokiStore = require('connect-loki')(session);
 
 require('dotenv').config();
 
@@ -34,14 +39,68 @@ const authValidation = [
   body('password', 'Password may not be longer than 128 characters').isLength({ max: 128 })
 ];
 
+passport.use(new LocalStrategy(async (username, password, done) => {
+  let user;
+  let match;
+
+  try {
+    user = await User.findOne({ username });
+
+    if (!user) return done(null, false);
+
+    match = await bcrypt.compare(password, user.password);
+
+    if (!match) return done(null, false);
+
+  } catch (err) {
+    return done(err);
+  }
+
+  return done(null, user);
+}));
+
 app.set('view engine', 'ejs');
 
+app.use(session({
+  genid: () => uuid(),
+  secret: process.env.TOKEN || require('crypto').randomBytes(48).toString('hex'),
+  store: new LokiStore(),
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.use(express.static('public'));
+
+app.get('/test', (req, res) => {
+  console.log(req.sessionID);
+  res.send('debugging\n');
+});
 
 app.get('/login.html', (req, res) => {
   const file = fs.readFileSync(path.join(__dirname, 'public', '.login.html.ejs')).toString();
   res.send(ejs.render(file, { errors: JSON.parse(req.query.errors || '[]'), serverError: req.query.serverError || '' }));
 });
+
+app.get('/home.html', (req, res) => {
+  
+});
+
+app.post('/login', urlencodedParser, [
+  ...authValidation
+], passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login.html' })
+);
 
 app.post('/register', urlencodedParser, [
   ...authValidation,
@@ -53,7 +112,7 @@ app.post('/register', urlencodedParser, [
     return true;
   })
 ], async (req, res) => {
-  const redirectUrl = req.query.redirectUrl || `/profile/${req.body.username}`;
+  const redirectUrl = req.query.redirectUrl || `/home`;
   const errorRedirectUrl = req.query.errorRedirectUrl || '/login.html';
   const errors = validationResult(req);
   const errorsJSON = JSON.stringify(errors.array().map(error => {
